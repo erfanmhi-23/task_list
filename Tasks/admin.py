58a2d2c8e3e8ad07f9096856ae8bf817c65538e7
Erfan <mhi.erfan.23@gamil.com>
2025-12-26 23:26:54 +0330

    admin done

--- Tasks/admin.py
+++ Tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tasks
 
-# Register your models here.
+@admin.register(Tasks)
+class taskadmin(admin.ModelAdmin):
+    list_display = ('owner','title','discription', 'deadline', 'is_archived')
+    list_filter = ('status', 'priority','is_archived')
+    search_fields = ('owner__user__username','title')
